bea3354ee9af1a51826f0eac06b39506514cf3e7
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-03-22 15:45:12 +0100

    Added password and username in config for mysql

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'RedBand-dfdfd-dfd'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    password = os.environ.get("MYSQL_PASSWORD")
+    username = os.environ.get("MYSQL_USER")
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + username + ':' + password + '@localhost/ffrs_ttn_map'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
