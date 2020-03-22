d04148296ab468e7d0b2930af2fc387ad1ea1578
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-03-22 16:39:45 +0100

    Implemented dotenv

--- app/config.py
+++ app/config.py
@@ -1,6 +1,16 @@
 import os
+import warnings
+from dotenv import load_dotenv
+from pathlib import Path
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+app_basedir = Path(__file__).parents[1]
+
+env_file = Path(app_basedir, ".env")
+if env_file.exists():
+    load_dotenv(env_file)
+else:
+    warnings.warn("No .env file found.")
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'RedBand-dfdfd-dfd'
