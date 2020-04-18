c1832c66fd61d0b8daa386ae85e1fc826b4bbac0
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-04-18 08:54:20 +0200

    Added fertig print

--- app/polygon_builder/polygon_builder.py
+++ app/polygon_builder/polygon_builder.py
@@ -99,6 +99,12 @@ def run_polygon_builder():
 
         db.session.commit()
 
-
+    print('''
+    ####    ####    #####    ########   ##   ####
+    ##      ##      ##  ##      ##      ##  ##
+    ####    ####    ####        ##      ##  ##  ###
+    ##      ##      ##  #       ##      ##  ##   ##
+    ##      ####    ##  ##      ##      ##   #####
+    ''')
     print('Alle Polygone gespeichert.')
 
