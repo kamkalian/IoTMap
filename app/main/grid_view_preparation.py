537631824b110b82370ff0959e7e70ebd807c177
kamkalian <kurm_de@kurm.de>
2020-08-02 09:20:21 +0200

    Decreased distance_in_a_circle

--- app/main/grid_view_preparation.py
+++ app/main/grid_view_preparation.py
@@ -10,7 +10,7 @@ class GridViewPreparation():
         self.r_pole = 6356.752
 
         # Entfernung um einen gemessenen Punkt
-        self.distance_in_a_circle = 10
+        self.distance_in_a_circle = 5
 
         # Entfernung zwischen den Breitenkreisen
         self.distance_circle_of_width = 111300
